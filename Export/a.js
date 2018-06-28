d0aa259958d1b13136886f5c684ca4026c3ab1d5
caoxudong02 <caoxudong02@meituan.com>
2018-06-28 12:27:37 +0800

    delete some useless file

--- Export/a.js
+++
@@ -1,4 +0,0 @@
-export var a = 'foo';
-
-setTimeout(()=>a = 'bar', 500);
-console.log('a.js:',a);
--- Export/b.js
+++
@@ -1,5 +0,0 @@
-import {a} from './a.js';
-
-console.log('b.js:',a);//output b.js: foo;
-setTimeout(()=>console.log('b.js after 500ms:'+a),500);//after 500ms output b.js after 500ms: bar;
-//Infact, change 500 to range 498 to 500 also works; 
