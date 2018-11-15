4ced4702b08ea8710024d3c14535d52ae24127ee
Xheldon <c1006044256@gmail.com>
2018-11-15 18:27:46 +0800

    add nuxt test

---
+++ nuxt-test/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        'plugin:vue/recommended'
+    ],
+    // required to lint *.vue files
+    plugins: [
+        'vue'
+    ],
+    // add your custom rules here
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': "off",
+        'vue/html-indent': ["error", 4],
+        'vue/script-indent': ["error", 4, {
+            'baseIndent': 1 // export default 不用 顶着头写
+        }]
+    }
+};
